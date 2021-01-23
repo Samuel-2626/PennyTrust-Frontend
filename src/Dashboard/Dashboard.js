b20aaf3ae0f55bb47793a557354e67afd1ec48d4
samuel <efosonaname@gmail.com>
2021-01-23 01:42:53 +0000

    Built UI for Apply Application

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,11 +1,52 @@
+
+
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+
 
 
 function Dashboard() {
   return (
     <div className="m-5">
       <Container>
-        <h1 className="text-center"><strong>Welcome to your Dashboard</strong></h1>
+        <h1 className="text-center"><strong>Login</strong></h1>
+
+        <Form autoComplete="off" className="mt-3">
+          <Container>
+            <Row>
+            <Col lg={12}>
+    
+          <Form.Group controlId="">
+            <Form.Control type="email" placeholder="email"  />
+            <Form.Text className="text-muted">
+            <small className="error"></small>
+            </Form.Text>
+          </Form.Group>
+          </Col>
+          <Col lg={12}>
+    
+          <Form.Group controlId="">
+            <Form.Control type="password" placeholder="password" />
+            <Form.Text className="text-muted">
+            <small className="error"></small>
+            </Form.Text>
+          </Form.Group>
+          </Col>
+        
+    
+          </Row>
+          </Container>
+         
+        </Form>
+
+        <Button variant="primary" className="btn-4">Login</Button>
+        <Button variant="primary" className="btn-4">forgot password</Button>
+
       </Container>
       
     </div>
