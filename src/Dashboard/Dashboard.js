a358fa5b21a330d94cff6418d550835ea14459b6
samuel <efosonaname@gmail.com>
2021-01-26 13:07:45 +0000

    Added API for registration

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -7,7 +7,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 import {UserContext} from '../UserContext';
-import axios from 'axios';
+// import axios from 'axios';
 
 
 
